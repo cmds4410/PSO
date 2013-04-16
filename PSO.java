dba7edd4dbb5161345391742929a270f85de7ef8
Connor Smith <cmds4410@gmail.com>
2013-04-16 19:40:45 -0400

    I think we're just about done for tomorrow

--- PSO.java
+++ PSO.java
@@ -5,5 +5,35 @@ import java.text.*;
 
 public class PSO
 {
+    
+}
 
+public void randomizeParticles()
+{
+    
+}
+
+public void updateVelocity(Particle p)
+{
+    
+}
+
+public void updatePosition(Particle p)
+{
+    
+}
+
+public void getValueForPosition(double position)
+{
+    
+}
+
+public void updatePersonalBest(Particle p)
+{
+    
+}
+
+public void updateGlobalBest(Particle p)
+{
+    
 }
---
+++ Particle.java
@@ -0,0 +1,14 @@
+public class Particle
+{
+    private double velocity;
+    private double position;
+    private double personalBest;
+    
+    public Particle(double v, double p) {
+        
+        velocity = v;
+        position = p;
+        
+    }
+    
+}
